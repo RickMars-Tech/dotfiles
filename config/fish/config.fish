4fb4bf112f2b6a646a46418dee0891844546dda2
RickMars <139187404+RickMars-Tech@users.noreply.github.com>
2024-02-24 19:57:08 -0600

    Add files via upload

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,9 +6,6 @@ set fish_greeting ""
 # Aliases
 alias grep "grep --color=auto"
 alias cat "bat --style=plain --paging=never"
-alias ls "exa --group-directories-first"
-alias tree "exa -T"
-
-# Prompt
-
-starship init fish | source
+alias ls "eza --all --group-directories-first --grid --icons"
+alias tree "eza -T --all --icons"
+alias ll "eza -l --all --octal-permissions --icons"
