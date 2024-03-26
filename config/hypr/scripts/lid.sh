d9c380d75d8f8b694538cdae3b571837c065c1c9
RickMars <139187404+RickMars-Tech@users.noreply.github.com>
2024-03-26 16:53:21 -0600

    Update lid.sh

--- config/hypr/scripts/lid.sh
+++ config/hypr/scripts/lid.sh
@@ -2,8 +2,8 @@
 
 if [[ "$(hyprctl monitors)" =~ "\sDP-[0-9]+" ]]; then
   if [[ $1 == "open" ]]; then
-    hyprctl keyword monitor "eDP-1,1920x1080,2560x0,1"
+    hyprctl keyword monitor "eDP-1, highres, auto, 1"
   else
-    hyprctl keyword monitor "eDP-1,disable"
+    hyprctl keyword monitor "eDP-1, disable"
   fi
 fi
